8df25ab10786f545ae4288ec592a7e4d83bc3ab0
Martin Zink <martinzink@apache.org>
2025-08-22 16:59:54 +0200

    wip

--- src/processors/simple_log_processor.rs
+++ src/processors/simple_log_processor.rs
@@ -5,10 +5,11 @@ use minifi_native::{
     Session, SessionFactory,
 };
 
+#[derive(Debug)]
 struct SimpleLogProcessor {
     logger: Logger,
     what_to_log: Option<String>,
-    what_to_log_property_name: String,
+    should_shout: bool,
 }
 
 impl SimpleLogProcessor {
@@ -17,38 +18,54 @@ impl SimpleLogProcessor {
         "FlowFiles are transferred here after logging",
     );
 
+    const WHAT_TO_LOG_PROPERTY: Property = Property {
+        name: "text",
+        description: "what to log",
+        is_required: false,
+        is_sensitive: false,
+        supports_expr_lang: false,
+        default_value: Some("Default text to log."),
+        validator: StandardPropertyValidator::AlwaysValidValidator,
+    };
+
+    const SHOUT_PROPERTY: Property = Property {
+        name: "Shouting",
+        description: "do you want to shout?",
+        is_required: true,
+        is_sensitive: false,
+        supports_expr_lang: false,
+        default_value: Some("false"),
+        validator: StandardPropertyValidator::BoolValidator,
+    };
 }
 
 impl Processor for SimpleLogProcessor {
     fn new(logger: Logger) -> Self {
-        Self { logger, what_to_log: None, what_to_log_property_name: "text".to_string() }
+        Self { logger, what_to_log: None, should_shout: false }
     }
 
     fn initialize(&mut self, descriptor: &mut Descriptor) {
         descriptor.set_supported_relationships(&[SimpleLogProcessor::SUCCESS_RELATIONSHIP]);
-        let what_to_log_property: Property = Property {
-            name: self.what_to_log_property_name.clone(),
-            description: "what to log".to_string(),
-            is_required: false,
-            is_sensitive: false,
-            supports_expr_lang: false,
-            default_value: Some("Default text to log.".to_string()),
-            validator: StandardPropertyValidator::AlwaysValidValidator,
-        };
-        descriptor.set_supported_properties(&[what_to_log_property])
+        descriptor.set_supported_properties(&[SimpleLogProcessor::WHAT_TO_LOG_PROPERTY, SimpleLogProcessor::SHOUT_PROPERTY]);
     }
 
     fn on_trigger(&mut self, _context: &ProcessContext, session: &mut Session) {
-        self.logger.info(format!("rusty on_trigger: {:?}", self.what_to_log).as_str());
+        let text_ref = self.what_to_log.as_ref().unwrap_or(&"".to_string()).clone();
+        let modified_text = if self.should_shout { text_ref.to_uppercase() } else { text_ref };
+        self.logger.info(format!("rusty on_trigger: {}", modified_text.as_str()).as_str());
 
-        if let Some(flow_file) = session.get() {
-            session.transfer(flow_file, "success");
-        }
+//        if let Some(flow_file) = session.get() {
+//            session.transfer(flow_file, "success");
+//        }
     }
 
     fn on_schedule(&mut self, context: &ProcessContext, _session_factory: &mut SessionFactory) {
-        self.what_to_log = context.get_property(self.what_to_log_property_name.as_str(), None);
-        self.logger.info(format!("rusty on_schedule: {:?}", self.what_to_log).as_str());
+        self.logger.info(format!("on_schedule entry {:?}", self).as_str());
+
+        self.what_to_log = context.get_property(SimpleLogProcessor::WHAT_TO_LOG_PROPERTY.name, None);
+        self.should_shout = context.get_property(SimpleLogProcessor::SHOUT_PROPERTY.name, None).and_then(|s| s.parse::<bool>().ok()).unwrap_or(false);
+
+        self.logger.info(format!("on_schedule exit {:?}", self).as_str());
     }
 
     fn get_name(&self) -> &'static str {
