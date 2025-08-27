9cfff8eb8eb7728a4429fdcfd2cec25859eb97db
Martin Zink <martinzink@apache.org>
2025-08-27 16:16:46 +0200

    compiles

--- src/processors/simple_log_processor.rs
+++ src/processors/simple_log_processor.rs
@@ -1,45 +1,40 @@
 use ctor::ctor;
 use minifi_native::ProcessorInputRequirement::Allowed;
-use minifi_native::{
-    LogLevel, Logger, ProcessContext, Processor, ProcessorBridge, Property, Relationship, Session,
-    SessionFactory, StandardPropertyValidator,
-};
+use minifi_native::{LogLevel, Logger, ProcessContext, Processor, ProcessorBridge, Property, ProcessSession, ProcessSessionFactory, StandardPropertyValidator, Relationship, CffiLogger};
 use strum::VariantNames;
 
 #[derive(Debug)]
-struct SimpleLogProcessor {
-    logger: Logger,
+struct SimpleLogProcessor<L: Logger> {
+    logger: L,
     log_level: LogLevel,
 }
 
-impl SimpleLogProcessor {
-    const SUCCESS_RELATIONSHIP: Relationship = Relationship {
-        name: "success",
-        description: "FlowFiles are transferred here after logging",
-    };
+const SUCCESS_RELATIONSHIP: Relationship = Relationship {
+    name: "success",
+    description: "FlowFiles are transferred here after logging",
+};
 
-    const LOG_LEVEL: Property = Property {
-        name: "Log Level",
-        description: "The level of logging.",
-        is_required: true,
-        is_sensitive: false,
-        supports_expr_lang: false,
-        default_value: Some("info"),
-        validator: StandardPropertyValidator::AlwaysValidValidator,
-        allowed_values: &LogLevel::VARIANTS,
-        allowed_types: &[],
-    };
-}
+const LOG_LEVEL: Property = Property {
+    name: "Log Level",
+    description: "The level of logging.",
+    is_required: true,
+    is_sensitive: false,
+    supports_expr_lang: false,
+    default_value: Some("info"),
+    validator: StandardPropertyValidator::AlwaysValidValidator,
+    allowed_values: &LogLevel::VARIANTS,
+    allowed_types: &[],
+};
 
-impl Processor for SimpleLogProcessor {
-    fn new(logger: Logger) -> Self {
+impl<L: Logger> Processor<L> for SimpleLogProcessor<L> {
+    fn new(logger: L) -> Self {
         Self {
             logger,
             log_level: LogLevel::Info,
         }
     }
 
-    fn on_trigger(&mut self, _context: &ProcessContext, session: &mut Session) {
+    fn on_trigger<P, S>(&mut self, _context: &P, session: &mut S) where P: ProcessContext, S: ProcessSession{
         self.logger
             .trace(format!("on_trigger entry {:?}", self).as_str());
 
@@ -49,7 +44,7 @@ impl Processor for SimpleLogProcessor {
              if let Some(content) = session.read(&input_ff) {
                  self.logger.log(self.log_level, content.as_str());
              }
-             session.transfer(input_ff, Self::SUCCESS_RELATIONSHIP.name);
+             session.transfer(input_ff, SUCCESS_RELATIONSHIP.name);
 
          }
 
@@ -57,12 +52,12 @@ impl Processor for SimpleLogProcessor {
             .trace(format!("on_trigger exit {:?}", self).as_str());
     }
 
-    fn on_schedule(&mut self, context: &ProcessContext, _session_factory: &mut SessionFactory) {
+    fn on_schedule<P, F>(&mut self, context: &P, _session_factory: &mut F) where P: ProcessContext, F: ProcessSessionFactory {
         self.logger
             .trace(format!("on_schedule entry {:?}", self).as_str());
 
         self.log_level = context
-            .get_property(SimpleLogProcessor::LOG_LEVEL.name, None)
+            .get_property(LOG_LEVEL.name, None)
             .and_then(|s| s.parse::<LogLevel>().ok())
             .unwrap_or(LogLevel::Info);
 
@@ -74,7 +69,7 @@ impl Processor for SimpleLogProcessor {
 #[ctor]
 #[no_mangle]
 fn register_simple_log_processor() {
-    let mut my_rust_processor = ProcessorBridge::<SimpleLogProcessor>::new(
+    let mut my_rust_processor = ProcessorBridge::<SimpleLogProcessor<CffiLogger>>::new(
         "rust_extension",
         "mzink.processors.rust.SimpleLogProcessor",
         "A rust processor that tests the upcoming C API, trying out most of the features",
@@ -84,9 +79,9 @@ fn register_simple_log_processor() {
     my_rust_processor.input_requirement = Allowed;
     my_rust_processor.supports_dynamic_properties = false;
     my_rust_processor.supports_dynamic_relationships = false;
-    my_rust_processor.relationships = vec![SimpleLogProcessor::SUCCESS_RELATIONSHIP];
+    my_rust_processor.relationships = vec![SUCCESS_RELATIONSHIP];
     my_rust_processor.properties = vec![
-        SimpleLogProcessor::LOG_LEVEL,
+        LOG_LEVEL,
     ];
 
     my_rust_processor.register_class();
--- src/processors/simple_source_processor.rs
+++ src/processors/simple_source_processor.rs
@@ -1,68 +1,63 @@
 use ctor::ctor;
 use minifi_native::ProcessorInputRequirement::{Forbidden};
-use minifi_native::{
-    Logger, ProcessContext, Processor, ProcessorBridge, Property, Relationship, Session,
-    SessionFactory, StandardPropertyValidator,
-};
+use minifi_native::{Logger, ProcessContext, Processor, ProcessorBridge, Property, ProcessSession, ProcessSessionFactory, StandardPropertyValidator, Relationship, CffiLogger};
 
 #[derive(Debug)]
-struct SimpleSourceProcessor {
-    logger: Logger,
+struct SimpleSourceProcessor<L: Logger> {
+    logger: L,
 }
 
-impl SimpleSourceProcessor {
-    const SUCCESS_RELATIONSHIP: Relationship = Relationship {
-        name: "success",
-        description: "FlowFiles are transferred here after logging",
-    };
+const SUCCESS_RELATIONSHIP: Relationship = Relationship {
+    name: "success",
+    description: "FlowFiles are transferred here after logging",
+};
 
-    const CONTENT_PROPERTY: Property = Property {
-        name: "Content",
-        description: "What to write to the flowfile.",
-        is_required: false,
-        is_sensitive: false,
-        supports_expr_lang: false,
-        default_value: Some("Something default to write"),
-        validator: StandardPropertyValidator::AlwaysValidValidator,
-        allowed_values: &[],
-        allowed_types: &[],
-    };
+const CONTENT_PROPERTY: Property = Property {
+    name: "Content",
+    description: "What to write to the flowfile.",
+    is_required: false,
+    is_sensitive: false,
+    supports_expr_lang: false,
+    default_value: Some("Something default to write"),
+    validator: StandardPropertyValidator::AlwaysValidValidator,
+    allowed_values: &[],
+    allowed_types: &[],
+};
 
-    const SHOUT_PROPERTY: Property = Property {
-        name: "Shouting",
-        description: "do you want to shout?",
-        is_required: true,
-        is_sensitive: false,
-        supports_expr_lang: false,
-        default_value: Some("false"),
-        validator: StandardPropertyValidator::BoolValidator,
-        allowed_values: &[],
-        allowed_types: &[],
-    };
-}
+const SHOUT_PROPERTY: Property = Property {
+    name: "Shouting",
+    description: "do you want to shout?",
+    is_required: true,
+    is_sensitive: false,
+    supports_expr_lang: false,
+    default_value: Some("false"),
+    validator: StandardPropertyValidator::BoolValidator,
+    allowed_values: &[],
+    allowed_types: &[],
+};
 
-impl Processor for SimpleSourceProcessor {
-    fn new(logger: Logger) -> Self {
+impl<L: Logger> Processor<L> for SimpleSourceProcessor<L> {
+    fn new(logger: L) -> Self {
         Self {
-            logger,
+            logger
         }
     }
 
-    fn on_trigger(&mut self, _context: &ProcessContext, session: &mut Session) {
+    fn on_trigger<P, S>(&mut self, _context: &P, session: &mut S) where P: ProcessContext, S: ProcessSession{
         self.logger
             .trace(format!("on_trigger exit {:?}", self).as_str());
 
         if let Some(new_ff) = session.create() {
             self.logger.info(format!("Created new flowfile").as_str());
             session.write(&new_ff, "Its just you and me");
-            session.transfer(new_ff, Self::SUCCESS_RELATIONSHIP.name);
+            session.transfer(new_ff, SUCCESS_RELATIONSHIP.name);
         }
 
         self.logger
             .trace(format!("on_trigger exit {:?}", self).as_str());
     }
 
-    fn on_schedule(&mut self, _context: &ProcessContext, _session_factory: &mut SessionFactory) {
+    fn on_schedule<P, F>(&mut self, _context: &P, _session_factory: &mut F) where P: ProcessContext, F: ProcessSessionFactory {
         self.logger
             .trace(format!("on_schedule entry {:?}", self).as_str());
 
@@ -74,7 +69,7 @@ impl Processor for SimpleSourceProcessor {
 #[ctor]
 #[no_mangle]
 fn register_simple_source_processor() {
-    let mut my_rust_processor = ProcessorBridge::<SimpleSourceProcessor>::new(
+    let mut my_rust_processor = ProcessorBridge::<SimpleSourceProcessor<CffiLogger>>::new(
         "rust_extension",
         "mzink.processors.rust.SimpleSourceProcessor",
         "A rust processor that acts as a source.",
@@ -84,10 +79,10 @@ fn register_simple_source_processor() {
     my_rust_processor.input_requirement = Forbidden;
     my_rust_processor.supports_dynamic_properties = false;
     my_rust_processor.supports_dynamic_relationships = false;
-    my_rust_processor.relationships = vec![SimpleSourceProcessor::SUCCESS_RELATIONSHIP];
+    my_rust_processor.relationships = vec![SUCCESS_RELATIONSHIP];
     my_rust_processor.properties = vec![
-        SimpleSourceProcessor::CONTENT_PROPERTY,
-        SimpleSourceProcessor::SHOUT_PROPERTY,
+        CONTENT_PROPERTY,
+        SHOUT_PROPERTY,
     ];
 
     my_rust_processor.register_class();
